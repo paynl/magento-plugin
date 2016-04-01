fd5b71facf259826ddf88a230aebea32841094cb
Andy Pieters <andy@pay.nl>
2016-04-01 15:47:48 +0200

    Added Payment Method Afterpay EM

---
+++ app/code/community/Pay/Payment/Block/Form/Afterpayem.php
@@ -0,0 +1,8 @@
+<?php
+
+class Pay_Payment_Block_Form_Afterpayem extends Pay_Payment_Block_Form_Abstract {
+
+    protected $paymentMethodId = Pay_Payment_Model_Paymentmethod_Afterpayem::OPTION_ID;
+    protected $paymentMethodName = 'AfterpayEM';
+
+}
---
+++ app/code/community/Pay/Payment/Model/Paymentmethod/Afterpayem.php
@@ -0,0 +1,8 @@
+<?php
+class Pay_Payment_Model_Paymentmethod_Afterpayem extends Pay_Payment_Model_Paymentmethod {
+    const OPTION_ID = 740;
+    protected $_paymentOptionId = 740;
+    protected $_code = 'pay_payment_afterpayem';
+    protected $_formBlockType = 'pay_payment/form_afterpayem';
+}
+    
