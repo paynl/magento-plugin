38ce29d01baf5cdb0dc8ebde2263b149eacdf951
Andy Pieters <andy@pay.nl>
2017-02-06 16:45:53 +0100

    Billink now always showing the agree with terms checkbox Billink personal setting defaults to yes

--- app/code/community/Pay/Payment/Block/Form/Billink.php
+++ app/code/community/Pay/Payment/Block/Form/Billink.php
@@ -12,11 +12,7 @@ class Pay_Payment_Block_Form_Billink extends Pay_Payment_Block_Form_Abstract {
         $enablePersonal = Mage::getStoreConfig('payment/pay_payment_billink/ask_data_personal', Mage::app()->getStore());
         $enableBusiness = Mage::getStoreConfig('payment/pay_payment_billink/ask_data_business', Mage::app()->getStore());
 
-        if($enablePersonal == 1 || $enableBusiness == 1){
-            $this->template ='pay/payment/form/billink.phtml';
-        } else {
-            $this->template = 'pay/payment/form/default.phtml';
-        }
+        $this->template ='pay/payment/form/billink.phtml';
 
         return  parent::_construct();
     }
