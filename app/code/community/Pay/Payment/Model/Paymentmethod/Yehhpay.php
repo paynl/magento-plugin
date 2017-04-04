377d3e77154649fe365fccd42c6e09a904e80667
Andy Pieters <andy@pay.nl>
2017-04-04 12:36:10 +0200

    Added yehhpay

---
+++ app/code/community/Pay/Payment/Model/Paymentmethod/Yehhpay.php
@@ -0,0 +1,11 @@
+<?php
+
+class Pay_Payment_Model_Paymentmethod_Yehhpay extends Pay_Payment_Model_Paymentmethod {
+
+    const OPTION_ID = 1877;
+
+    protected $_paymentOptionId = 1877;
+    protected $_code = 'pay_payment_yehhpay';
+    protected $_formBlockType = 'pay_payment/form_yehhpay';
+
+}
