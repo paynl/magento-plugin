32e76388abbd7f4c1d32936516f9174b31dfcb80
Andy Pieters <andy@pay.nl>
2017-06-20 15:10:41 +0200

    Added payment method Fashion giftcard

---
+++ app/code/community/Pay/Payment/Block/Form/Fashiongiftcard.php
@@ -0,0 +1,10 @@
+<?php
+
+class Pay_Payment_Block_Form_Fashiongiftcard extends Pay_Payment_Block_Form_Abstract {
+
+    protected $paymentMethodId = Pay_Payment_Model_Paymentmethod_Fashiongiftcard::OPTION_ID;
+    protected $paymentMethodName = 'Fashiongiftcard';
+
+    protected $methodCode = 'pay_payment_fashiongiftcard';
+
+}
---
+++ app/code/community/Pay/Payment/Model/Paymentmethod/Fashiongiftcard.php
@@ -0,0 +1,8 @@
+<?php
+class Pay_Payment_Model_Paymentmethod_Fashiongiftcard extends Pay_Payment_Model_Paymentmethod {
+    const OPTION_ID = 1669;
+    protected $_paymentOptionId = 1669;
+    protected $_code = 'pay_payment_fashiongiftcard';
+    protected $_formBlockType = 'pay_payment/form_fashiongiftcard';
+}
+    
