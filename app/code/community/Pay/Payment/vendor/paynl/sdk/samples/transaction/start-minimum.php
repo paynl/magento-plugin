0e306e7226b0500145b3229dc20614e862ca69d1
Andy Pieters <andy@pay.nl>
2018-02-22 16:52:55 +0100

    Updated the SDK

--- app/code/community/Pay/Payment/vendor/paynl/sdk/samples/transaction/start-minimum.php
+++ app/code/community/Pay/Payment/vendor/paynl/sdk/samples/transaction/start-minimum.php
@@ -1,20 +1,5 @@
 <?php
-/*
- * Copyright (C) 2015 Andy Pieters <andy@pay.nl>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+
 
 require_once '../../vendor/autoload.php';
 require_once '../config.php';
@@ -23,11 +8,11 @@ try {
     $result = \Paynl\Transaction::start(array(
     	// Required
         'amount' => 12.5,
-        'returnUrl' => dirname(Paynl\Helper::getBaseUrl()) . '/return.php',
+        'returnUrl' => dirname(\Paynl\Helper::getBaseUrl()) . '/return.php',
         'ipaddress' => \Paynl\Helper::getIp(),
 
 	    // Optional
-        'exchangeUrl' => dirname(Paynl\Helper::getBaseUrl()) . '/exchange.php',
+        'exchangeUrl' => dirname(\Paynl\Helper::getBaseUrl()) . '/exchange.php',
         'paymentMethod' => 10,// iDEAL use \Paynl\PaymentMethods::getList() to get all available paymentmethods
         'description' => '123456', // the transaction description, usually the orderId
     ));
--- app/code/community/Pay/Payment/vendor/paynl/sdk/samples/transaction/start.php
+++ app/code/community/Pay/Payment/vendor/paynl/sdk/samples/transaction/start.php
@@ -23,22 +23,23 @@ try {
     $result = \Paynl\Transaction::start(array(
         // required
         'amount' => 12.5,
-        'returnUrl' => dirname(Paynl\Helper::getBaseUrl()) . '/return.php',
+        'returnUrl' => dirname(\Paynl\Helper::getBaseUrl()) . '/return.php',
 
         // optional
-        'exchangeUrl' => dirname(Paynl\Helper::getBaseUrl()) . '/exchange.php',
+        'exchangeUrl' => dirname(\Paynl\Helper::getBaseUrl()) . '/exchange.php',
         'paymentMethod' => 10,
         'currency' => 'EUR',
-        'expireDate' => new DateTime('2016-04-01'),
+        'expireDate' => new \DateTime('2016-04-01'),
 //        'bank' => 1,
+        'orderNumber' => 'ABCDEFG',
         'description' => '123456',
         'testmode' => 0,
         'extra1' => 'ext1',
         'extra2' => 'ext2',
         'extra3' => 'ext3',
         'ipaddress' => '10.0.0.1',
-        'invoiceDate' => new DateTime('now'),
-        'deliveryDate' => new DateTime('2016-06-06'), // in case of tickets for an event, use the event date here
+        'invoiceDate' => new \DateTime('now'),
+        'deliveryDate' => new \DateTime('2016-06-06'), // in case of tickets for an event, use the event date here
         'products' => array(
             array(
                 'id' => 1,
@@ -86,7 +87,7 @@ try {
             'initials' => 'T',
             'lastName' => 'Test',
             'gender' => 'M',
-            'birthDate' => new DateTime('1999-02-15'),
+            'birthDate' => new \DateTime('1999-02-15'),
             'phoneNumber' => '0612345678',
             'emailAddress' => 'test@test.nl',
             'customerReference' => '456789',//your customer id
