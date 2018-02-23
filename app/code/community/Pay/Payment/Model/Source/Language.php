afd584e169a4ef00aa2561658d5ad3ee52fed89d
Andy Pieters <andy@pay.nl>
2018-02-23 14:45:09 +0100

    Added more languages to the payment screen language setting

--- app/code/community/Pay/Payment/Model/Source/Language.php
+++ app/code/community/Pay/Payment/Model/Source/Language.php
@@ -9,13 +9,19 @@ class Pay_Payment_Model_Source_Language {
     public function toOptionArray() {
 
         return array(
-            array('value' => 'nl', 'label' => 'Nederlands'),
-            array('value' => 'en', 'label' => 'Engels'),
-            array('value' => 'de', 'label' => 'Duits'),
-            array('value' => 'es', 'label' => 'Spaans'),
-            array('value' => 'it', 'label' => 'Italiaans'),
-            array('value' => 'fr', 'label' => 'Frans'),
-            array('value' => 'browser', 'label' => 'Browser taal gebruiken'),            
+            array('value' => 'nl', 'label' => Mage::helper('core')->__('Nederlands')),
+            array('value' => 'en', 'label' => Mage::helper('core')->__('Engels')),
+            array('value' => 'de', 'label' => Mage::helper('core')->__('Duits')),
+            array('value' => 'es', 'label' => Mage::helper('core')->__('Spaans')),
+            array('value' => 'it', 'label' => Mage::helper('core')->__('Italiaans')),
+            array('value' => 'fr', 'label' => Mage::helper('core')->__('Frans')),
+            array('value' => 'da', 'label' => Mage::helper('core')->__('Deens')),
+            array('value' => 'no', 'label' => Mage::helper('core')->__('Noors')),
+            array('value' => 'pt', 'label' => Mage::helper('core')->__('Portugees')),
+            array('value' => 'ru', 'label' => Mage::helper('core')->__('Russisch')),
+            array('value' => 'sv', 'label' => Mage::helper('core')->__('Zweeds')),
+            array('value' => 'tr', 'label' => Mage::helper('core')->__('Turks')),
+            array('value' => 'browser', 'label' => Mage::helper('core')->__('Browser taal gebruiken')),
         );
     }
 
