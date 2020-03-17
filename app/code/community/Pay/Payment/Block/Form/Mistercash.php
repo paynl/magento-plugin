77e30f061d3426372f556bacaa78419763df8723
Max Geraci <max@pay.nl>
2020-03-17 14:42:36 +0100

    Change Bancontact/Mistercash name to Bancontact

--- app/code/community/Pay/Payment/Block/Form/Mistercash.php
+++ app/code/community/Pay/Payment/Block/Form/Mistercash.php
@@ -3,7 +3,7 @@
 class Pay_Payment_Block_Form_Mistercash extends Pay_Payment_Block_Form_Abstract {
 
     protected $paymentMethodId = Pay_Payment_Model_Paymentmethod_Mistercash::OPTION_ID;
-    protected $paymentMethodName = 'Bancontact/Mistercash';
+    protected $paymentMethodName = 'Bancontact';
 
     protected $methodCode = 'pay_payment_mistercash';
 
