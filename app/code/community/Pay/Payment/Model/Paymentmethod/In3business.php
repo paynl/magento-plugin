fd549589abd32bb12fdabc245539e2a63a4d1aa6
Anne <anne@pay.nl>
2024-08-20 11:56:46 +0200

    Add in3 Business

---
+++ app/code/community/Pay/Payment/Model/Paymentmethod/In3business.php
@@ -0,0 +1,7 @@
+<?php
+class Pay_Payment_Model_Paymentmethod_In3business extends Pay_Payment_Model_Paymentmethod {
+    const OPTION_ID = 3192;
+    protected $_paymentOptionId = 3192;
+    protected $_code = 'pay_payment_in3business';
+    protected $_formBlockType = 'pay_payment/form_in3business';
+}
