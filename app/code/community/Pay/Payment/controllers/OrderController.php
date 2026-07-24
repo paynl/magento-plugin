26c8dd2e0d8d1ac2fab353511f405a6c8127a8cb
Kevin Verschoor <k.verschoor@pay.nl>
2026-07-24 12:32:55 +0200

    PLUG-5633 - Prevent error when called directly

--- app/code/community/Pay/Payment/controllers/OrderController.php
+++ app/code/community/Pay/Payment/controllers/OrderController.php
@@ -26,21 +26,31 @@ class Pay_Payment_OrderController extends Mage_Core_Controller_Front_Action
 
         $transactionId = $params['orderId'];
 
-        $status = $this->helperOrder->getTransactionStatus($transactionId);
-        $order = $this->helperOrder->getOrderByTransactionId($transactionId);
-        $store = $order->getStore();
+        if (empty($transactionId) || empty($status) || empty($order)) {
+            Mage::log('Error in returnAction, cannot find transactionId', null, 'return.log');
+            $this->_redirect('checkout/cart');
+        }
+
+        try {
+            $status = $this->helperOrder->getTransactionStatus($transactionId);
+            $order = $this->helperOrder->getOrderByTransactionId($transactionId);
+            $store = $order->getStore();
+        } catch (\Throwable $e) {
+            Mage::log('Error in returnAction, ' . $e->getMessage(), null, 'return.log');
+            $this->_redirect('checkout/cart');
+        }
 
         $extended_logging = Mage::getStoreConfig('pay_payment/general/extended_logging', $store);
 
         $pageSuccess = $store->getConfig('pay_payment/general/page_success');
         $pagePending = $store->getConfig('pay_payment/general/page_pending');
         $pageCanceled = $store->getConfig('pay_payment/general/page_canceled');
-
+        
 	    /**
 	     * @var $orderPayment Mage_Sales_Model_Order_Payment
 	     */
 	    $orderPayment = $order->getPayment();
-	    $hash = $orderPayment->getAdditionalInformation( 'paynl_hash');
+	    $hash = $orderPayment->getAdditionalInformation('paynl_hash');
 	    if(!empty($hash)){
 	    	$instoreStatus = \Paynl\Instore::status(array(
 	    		'hash' => $hash
