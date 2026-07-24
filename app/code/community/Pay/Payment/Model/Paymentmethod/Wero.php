e0222ed393dcb8ec3a6637c78fd22f6699181afe
Kevin Verschoor <k.verschoor@pay.nl>
2026-07-24 12:00:08 +0200

    PLUG-5995 - Add WERO

---
+++ app/code/community/Pay/Payment/Model/Paymentmethod/Wero.php
@@ -0,0 +1,8 @@
+<?php
+class Pay_Payment_Model_Paymentmethod_Wero extends Pay_Payment_Model_Paymentmethod
+{
+    const OPTION_ID = 3762;
+    protected $_paymentOptionId = 3762;
+    protected $_code = 'pay_payment_wero';
+    protected $_formBlockType = 'pay_payment/form_wero';
+}
